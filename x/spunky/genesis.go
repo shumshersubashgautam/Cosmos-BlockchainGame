b4c655bf0f8565bfeb25ff3a8f15f1a5ff7cd58b
Developer Experience team at Tendermint <hello@tendermint.com>
2022-06-12 09:54:50 +0545

    Initialized with Starport

---
+++ x/spunky/genesis.go
@@ -0,0 +1,26 @@
+package spunky
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/singhhp1069/spunky/x/spunky/keeper"
+	"github.com/singhhp1069/spunky/x/spunky/types"
+)
+
+// InitGenesis initializes the capability module's state from a provided genesis
+// state.
+func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
+	// this line is used by starport scaffolding # genesis/module/init
+
+	// this line is used by starport scaffolding # ibc/genesis/init
+}
+
+// ExportGenesis returns the capability module's exported genesis.
+func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
+	genesis := types.DefaultGenesis()
+
+	// this line is used by starport scaffolding # genesis/module/export
+
+	// this line is used by starport scaffolding # ibc/genesis/export
+
+	return genesis
+}
